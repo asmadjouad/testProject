78c1006283ad08288793ea09c4b6b8775948fe7c
asmapc <asmapc@10.115.50.92>
2022-03-02 11:42:16 +0100

    updating TestClass

--- testProject/src/testProject/TestClass.java
+++ testProject/src/testProject/TestClass.java
@@ -1,5 +1,19 @@
 package testProject;
 
 public class TestClass {
+int field;
+
+public TestClass(int field) {
+	super();
+	this.field = field;
+}
+
+public int getField() {
+	return field;
+}
+
+public void setField(int field) {
+	this.field = field;
+}
 
 }
